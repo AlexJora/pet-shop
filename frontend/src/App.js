aed3cc0fc2ecbdeb951d137c18921f41e0250dc0
AlexJora <jora.catalinaa@gmail.com>
2023-05-28 15:47:23 +0200

    react router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,27 @@
-import React from 'react';
-import { Container } from 'react-bootstrap';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import HomeScreen from './screens/HomeScreen';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
 const App = () => {
   return (
-    <>
-     <Header/>
-     <Container>
-     <main className='py-4'>
-      <HomeScreen/>
-     </main>
-     </Container>
-     <Footer/> 
-    </>
-  )
-}
+    <Router>
+      <Header />
 
-export default App
+      <main className="py-4">
+        <Container>
+          <Routes>
+            <Route path="/" element={<HomeScreen />} />
+            <Route path="/product/:id" element={<ProductScreen />} />
+          </Routes>
+        </Container>
+      </main>
+
+      <Footer />
+    </Router>
+  );
+};
+
+export default App;
