b502191881c74e6865cc1d7785d0cafa555a80bf
AlexJora <jora.catalinaa@gmail.com>
2023-05-29 18:38:50 +0200

    express server + initial routes

---
+++ backend/data/products.js
@@ -0,0 +1,108 @@
+const products = [
+  {
+    _id: "1",
+    name: "ROYAL CANIN® Kitten ",
+    image: "/images/product1.jpg",
+    description:
+      "Royal Canin Kitten Dry Food gives your kitten a healthy start with nutritional precision: An innovative mix of highly digestible proteins, vitamins, and other essential minerals support their maturing digestive system and keeps their growing bodies strong.",
+    brand: "Royal Canin",
+    category: "dry cat food",
+    price: 89.99,
+    countInStock: 10,
+    rating: 4.5,
+    numReviews: 12,
+  },
+  {
+    _id: "2",
+    name: "KIT CAT DRY ADULT CAT FOOD CLASSIC 32",
+    image: "/images/product2.jpg",
+    description:
+      "Treat your cat to a premium cat diet created by nutritionists (who are cat lovers) to provide your kitty cat with a meal made with carefully selected ingredients loaded with benefits.This premium meal is packed with beneficial ingredients to reduce digestive and hairball issues; and promote skin, coat, eye and heart health too.",
+    brand: "",
+    category: "dry cat food",
+    price: 250,
+    countInStock: 7,
+    rating: 4.0,
+    numReviews: 8,
+  },
+  {
+    _id: "3",
+    name: "Grain Zero Signature ",
+    image: "/images/product3.jpg",
+    description:
+      "Hypoallergenic complete nutrition cat food Great for adult cats of all breeds Made with real ocean fish, sardine and mackerel Balanced calories for a healthy body weight Includes 48% meat",
+    brand: "Signature",
+    category: "dry cat food",
+    price: 200,
+    countInStock: 5,
+    rating: 3,
+    numReviews: 12,
+  },
+  {
+    _id: "4",
+    name: "Friskies Dry Cat Food",
+    image: "/images/product4.jpg",
+    description:
+      "Get your cat purring at mealtime with the tempting texture combination found in Purina Friskies Tender and Crunchy Combo dry adult cat food. Meaty tenders and crunchy bits give her more to love in every bite, and the pleasing shapes get her excited to nibble her way to a happy belly. ",
+    brand: "Purina",
+    category: "dry cat food",
+    price: 300,
+    countInStock: 11,
+    rating: 5,
+    numReviews: 12,
+  },
+  {
+    _id: "5",
+    name: "Optimizor Premium",
+    image: "/images/product5.png",
+    description:
+      "Ideal Protein Sources: This formula is made with fresh chicken as the number one ingredient. Chicken and egg proteins are highly digestible and ideal",
+    brand: "Optimizor",
+    category: "dry dog food",
+    price: 49.99,
+    countInStock: 7,
+    rating: 3.5,
+    numReviews: 10,
+  },
+  {
+    _id: "6",
+    name: "Hill adult mature medium size",
+    image: "/images/product6.jpg",
+    description:
+      "Hill Science Plan Canine Mature Adult 7+ Active Longevity Medium with Chicken is formulated to sustain mobility and healthy vital organs. With clinically proven antioxidants, and glucosamine and chondroitin from natural sources.",
+    brand: "Hill",
+    category: "dry dog food",
+    price: 457.99,
+    countInStock: 0,
+    rating: 4,
+    numReviews: 12,
+  },
+  {
+    _id: "7",
+    name: "Hill adult Large Breed",
+    image: "/images/product7.jpg",
+    description:
+      "Hill Science Plan Canine Mature Adult 7+ Active Longevity Medium with Chicken is formulated to sustain mobility and healthy vital organs. With clinically proven antioxidants, and glucosamine and chondroitin from natural sources.",
+    brand: "Hill",
+    category: "dry dog food",
+    price: 299.99,
+    countInStock: 0,
+    rating: 4,
+    numReviews: 12,
+  },
+  {
+    _id: "8",
+    name: "Pedigree Adult for Medium Breed Size Dogs",
+    image: "/images/product8.jpg",
+    description:
+      "Why Pedigree Dry Adult Food?At Pedigree we believe that every dog deserves a loving home, and that every dog deserves leading nutrition. From over 40 years experience of developing Pedigree recipes, we believe that dogs have four universal needs: Skin & Coat, Immunity, Digestion and Oral Care. That is why Pedigree Vital Protection has been designed to deliver nutrition that addresses these four needs: the product helps support a strong immune system, a healthy skin and coat, and good Digestion as well as helping to clean the dog teeth.",
+    brand: "Pedigree",
+    category: "dry dog food",
+    price: 349.99,
+    countInStock: 0,
+    rating: 4,
+    numReviews: 12,
+  },
+];
+
+export default products;
