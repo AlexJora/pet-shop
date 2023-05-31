4dfb1e1f41bae20944005913c4cbd2c15f540867
AlexJora <jora.catalinaa@gmail.com>
2023-05-31 14:21:05 +0200

    creating file and adding asyncHandler in productRoutes

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,11 +1,18 @@
 import express from "express";
 const router = express.Router();
-import products from "../data/products.js";
-router.get("/", (req, res) => {
-  res.json(products);
-});
-router.get("/:id", (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
+import asyncHandler from "../middleware/asyncHandler";
+//adding 'async handler' cos mongoose methods are async
+router.get(
+  "/",
+  asyncHandler(async (req, res) => {
+    res.json(products);
+  })
+);
+router.get(
+  "/:id",
+  asyncHandler(async (req, res) => {
+    const product = products.find((p) => p._id === req.params.id);
+    res.json(product);
+  })
+);
 export default router;
