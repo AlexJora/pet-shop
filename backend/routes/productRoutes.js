d3abcbcfdc4523cc89bb5728ff404ed8f84a41c2
AlexJora <jora.catalinaa@gmail.com>
2023-05-31 14:28:11 +0200

    fetching products and product by id from data base

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,18 +1,24 @@
 import express from "express";
 const router = express.Router();
 import asyncHandler from "../middleware/asyncHandler";
+import Product from "../models/productModel.js";
 //adding 'async handler' cos mongoose methods are async
 router.get(
   "/",
   asyncHandler(async (req, res) => {
+    //fetching products from DB
+    const products = await Product.find({});
     res.json(products);
   })
 );
 router.get(
   "/:id",
   asyncHandler(async (req, res) => {
-    const product = products.find((p) => p._id === req.params.id);
-    res.json(product);
+    const product = await Product.findById(req.params.id);
+    if (product) {
+      return res.json(product);
+    }
+    res.status(404).json({ message: "Product not found" });
   })
 );
 export default router;
