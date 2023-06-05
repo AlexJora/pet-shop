cfbfbef189780393449ea672f1ae9b09805f560e
AlexJora <jora.catalinaa@gmail.com>
2023-06-06 00:08:54 +0200

    calculations

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -4,6 +4,9 @@ const initialState = localStorage.getItem("cart")
   ? JSON.parse(localStorage.getItem("cart"))
   : { cartItems: [] };
 
+const addDecimals = (num) => {
+  return (Math.round(num * 100) / 100).toFixed(2);
+};
 const cartSlice = createSlice({
   name: "cart",
   initialState,
@@ -21,7 +24,29 @@ const cartSlice = createSlice({
         //add new item
         state.cartItems = [...state.cartItems];
       }
+      //Calculate items price
+      state.itemsPrice = addDecimals(
+        state.cartItems.reduce((acc, item) => acc + item.price * item.qty, 0)
+      );
+      //Calculate shipping price
+      state.shippingPrice = addDecimals(state.itemsPrice > 100 ? 0 : 10);
+
+      //Calculate tax price (15% tax)
+      state.taxPrice = addDecimals(
+        Number((0.15 * state.itemsPrice).toFixed(2))
+      );
+
+      //Calculate total price
+      state.totalPrice = (
+        Number(state.itemsPrice) +
+        Number(state.shippingPrice) +
+        Number(state.taxPrice)
+      ).toFixed(2);
+
+      localStorage.setItem("cart", JSON.stringify(state));
     },
   },
 });
+
+export const { addToCart } = cartSlice.actions;
 export default cartSlice.reducer;
