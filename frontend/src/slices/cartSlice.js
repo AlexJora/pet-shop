2e2e8040d5530818e7f87e8622579e808bd39615
AlexJora <jora.catalinaa@gmail.com>
2023-06-05 23:39:54 +0200

    starting addToCart function

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -7,6 +7,21 @@ const initialState = localStorage.getItem("cart")
 const cartSlice = createSlice({
   name: "cart",
   initialState,
-  reducers: {},
+  reducers: {
+    addToCart: (state, action) => {
+      const item = action.payload;
+      //checking if the item exist in the cart already
+      const existItem = state.cartItems.find((x) => x._id === item._id);
+      //update quantity
+      if (existItem) {
+        state.cartItems = state.cartItems.map((y) =>
+          y._id === existItem._id ? item : y
+        );
+      } else {
+        //add new item
+        state.cartItems = [...state.cartItems];
+      }
+    },
+  },
 });
 export default cartSlice.reducer;
