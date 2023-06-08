1381db7ac792749d9f1282228e7183b6b49f055c
AlexJora <jora.catalinaa@gmail.com>
2023-06-09 00:18:32 +0200

    userRoutes.js

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import connectDB from "./config/db.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 //import product routes
 import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 const port = process.env.PORT;
 connectDB();
 const app = express();
@@ -12,8 +13,12 @@ const app = express();
 app.get("/", (req, res) => {
   res.send("API is running....");
 });
+
 //path for product routes (in routes / + /:id)
 app.use("/api/products", productRoutes);
+//path for users routes (in routes / + /:id)
+app.use("/api/users", userRoutes);
+
 app.use(notFound);
 app.use(errorHandler);
 app.listen(port, () => console.log(`Server running on port ${port}`));
