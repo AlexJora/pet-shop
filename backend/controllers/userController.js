8f89ee3c112f3d5e768370b02cd2039ac14d9543
AlexJora <jora.catalinaa@gmail.com>
2023-06-26 21:55:14 +0200

    authSlice

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -68,7 +68,6 @@ const registerUser = asyncHandler(async (req, res) => {
 //@route POST/api/users/logout
 //@access Private
 const logoutUser = asyncHandler(async (req, res) => {
-  console.log("logout");
   res.cookie("jwt", "", { httpOnly: true, expires: new Date(0) });
   res.status(200).json({ message: "Logged out successfully" });
 });
