937cfc93cc8715702a41808b2a168316f4285db9
AlexJora <jora.catalinaa@gmail.com>
2023-06-27 11:45:19 +0200

    LoginScreen

---
+++ frontend/src/screens/LoginScreen.js
@@ -0,0 +1,59 @@
+import React from "react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import {
+  Form,
+  Button,
+  Row,
+  Col,
+  FormGroup,
+  FormLabel,
+  FormControl,
+} from "react-bootstrap";
+import FormContainer from "../components/FormContainer";
+
+const LoginScreen = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log("submit");
+  };
+  return (
+    <FormContainer>
+      <h1>Sign In</h1>
+      <Form>
+        <FormGroup controlId="email" className="my-3">
+          <FormLabel>Email Adress</FormLabel>
+          <Form.Control
+            type="email"
+            placeholder="enter email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          ></Form.Control>
+        </FormGroup>
+        <FormGroup controlId="password" className="my-3">
+          <FormLabel>Password</FormLabel>
+          <Form.Control
+            type="password"
+            placeholder="enter password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          ></Form.Control>
+        </FormGroup>
+        <Button type="submit" variant="primary" className="mt-2">
+          Sign In
+        </Button>
+
+        <Row className="py-3">
+          <Col>
+            New Customer? <Link to="/register">Register</Link>
+          </Col>
+        </Row>
+      </Form>
+    </FormContainer>
+  );
+};
+
+export default LoginScreen;
